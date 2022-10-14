fca020851cc2b046f7d9468bd7180d6296dd8758
Fatih SOYSAL <106076235+fsoysall@users.noreply.github.com>
2022-10-15 01:39:52 +0300

    Delete day1 directory

--- day1/src/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ className = "btn btn-default", ...props }) {
-	return (
-		<button onClick={props.onClick} className={className}>
-			{props.children}
-		</button>
-	);
-}
