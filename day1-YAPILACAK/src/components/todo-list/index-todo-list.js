d5fa6764d362fe45a9d1a68f0670ab622e24ea12
fsoysall <fsoysal@gmail.com>
2022-10-15 01:02:46 +0300

    ALL: OK ( DAY1 , Dark-Light , Custom Event )

--- day1-YAPILACAK/src/components/todo-list/index-todo-list.js
+++ day1-YAPILACAK/src/components/todo-list/index-todo-list.js
@@ -10,11 +10,13 @@ const TodoList = () => {
 
 	//  0: YOK | n0: Asc, n1: DeAsc 
 	// 10, 11 : a.title  | 20, 21 : a.id  | 30, 31 : a.completed
-	let SiralayisKriteri = 0
+	// let setSiralayisKriteri = (0
 	// let aktifSayfa = 1
-	let SES = 5 // SayfaElemanSayısı 
+	// let ses = 5 // SayfaElemanSayısı 
 	let _todosPaged = {}
 
+	const [siralayisKriteri, setSiralayisKriteri] = useState(0);
+	const [ses, setSES] = useState(8);
 	const [aktifSayfa, setAktifSayfa] = useState(1);
 	const [todos, setTodos] = useState([]);
 	const [selectedTodo, setSelectedTodo] = useState(/*{ id: null, completed: false, title: "" }*/);
@@ -35,11 +37,12 @@ const TodoList = () => {
 	}, []);
 
 
-	const remove = (todo) => { if (window.confirm("Silmek üzerisiniz emin misiniz")) { setTodos(prev => { return prev.filter(x => x.id != todo.id) }) } }
+	const remove = (todo) => { if (window.confirm("Silmek üzerisiniz emin misiniz")) { setTodos(prev => { return prev.filter(x => x.id !== todo.id) }) } }
 
 	const todosPaged = () => {
-		console.log("	todosPaged", Baslangic + SES, Baslangic, SES);
-		_todosPaged = todos.slice(Baslangic, Baslangic + SES)
+		console.log("	todosPaged", Baslangic + ses, Baslangic, ses);
+		SiralamaAssist()
+		_todosPaged = todos.slice(Baslangic, Baslangic + ses)
 		// debugger
 		return _todosPaged
 	}
@@ -121,7 +124,7 @@ const TodoList = () => {
 	//#endregion EDIT ACTIONS
 
 
-	const Baslangic = (aktifSayfa - 1) * SES; /* 4.sayfa * 5 eleman = 20 */
+	const Baslangic = (aktifSayfa - 1) * ses; /* 4.sayfa * 5 eleman = 20 */
 
 	const renderToDoS = () => {
 		console.log("renderToDoS");
@@ -130,8 +133,7 @@ const TodoList = () => {
 			<div className={`${classes.container} container`}>
 				{id !== undefined && id != null && renderEditForm()}
 
-				{/* {SiralamaAssist()} */}
-				<table className="table table-hover" >
+				<table id="tTodos" className="table table-hover" >
 					{renderThead()}
 					{renderBody()}
 				</table>
@@ -152,13 +154,16 @@ const TodoList = () => {
 						console.log("todosPaged().map", index);
 						return (
 							<tr key={index}>
-								<td>
-									<Button onClick={() => remove(todo)} className={`btn btn-sm btn-danger ${classes.actionButton} `}>SİL</Button>
-									{todo.id} : {todo.userId}
+								<td style={{ minWidth: "2cm" }}>
+									<Button onClick={() => remove(todo)} className={`btn btn-sm btn-danger ${classes.actionButton} `}> SİL : {todo.id} </Button>
+									{/* {todo.id} : {todo.userId} */}
+								</td>
+								<td style={{ minWidth: "2cm" }}>
+									{todo.userId}
 								</td>
 								{/* <td style={{ width: "2cm", borderRadius: "10px" }} className={todo.completed ? "text-bg-success" : "text-bg-warning"} >{todo.completed ? "OK" : "..."}</td> */}
-								<td>{todo.title}</td>
-								<td >
+								<td style={{ minWidth: "12cm" }}>{todo.title}</td>
+								<td style={{ minWidth: "4cm" }}>
 									<Button style={{ borderRadius: "30px !important" }} className={"button50px " + (todo.completed ? "btn btn-success" : "btn btn-warning")} >{todo.completed ? "OK" : "..."}</Button>
 									<Button onClick={() => edit(todo)} className="btn btn-sm btn-warning">Düzenle</Button>
 								</td>
@@ -171,13 +176,25 @@ const TodoList = () => {
 		);
 	};
 
+	const PageSizeChanged = (e) => { let v = e.target.value; setSES(parseInt(v)) }
+
 	const renderThead = () => {
 		return (
 			<thead >
 				<tr>
-					<th ><span class="badge bg-primary"> Rn : U.Rn</span> </th>
-					<th ><span class="badge bg-primary"> İÇERİK</span></th>
-					<th ><span class="badge bg-primary"> İŞLEM</span></th>
+					<th scope="col" style={{ width: "2cm" }} id="idSiralamaKriteri_Id" onClick={() => SiralamaKriteri_Id(this)} 		>Id</th>
+					<th scope="col" style={{ width: "2cm" }} id="idSiralamaKriteri_KId" onClick={() => SiralamaKriteri_KId(this)} 	>Şahıs Id</th>
+					<th scope="col" style={{ width: "2cm" }} id="idSiralamaKriteri_Baslik" onClick={() => SiralamaKriteri_Baslik(this)} 	>Başlık</th>
+					{/* <th scope="col" style={{ width: "4cm" }} id="idSiralamaKriteri_Durum" 	onClick="SiralamaKriteri_Durum(this)} 	>Durum</th> */}
+					<th scope="col" style={{ width: "5cm" }} id="idSiralamaKriteri_Durum" onClick={() => SiralamaKriteri_Durum(this)} 	>
+						<select name="namePageSize" id="idPageSize" onChange={() => PageSizeChanged}>
+							<option> 8 </option>
+							<option> 10</option>
+							<option> 25</option>
+							<option> 50</option>
+						</select> &nbsp; İşlem
+					</th>
+
 				</tr>
 			</thead>
 		);
@@ -186,38 +203,26 @@ const TodoList = () => {
 
 	//#region "pager"
 
-	/* export  */  const aktifSayfaDegis = (YeniSayfa) => {
+	const aktifSayfaDegis = (YeniSayfa) => {
 		console.log(`YS: ${YeniSayfa} | AS: ${aktifSayfa}`);
 		setAktifSayfa(YeniSayfa % todos.length);
-		console.log(`YS: ${YeniSayfa} | AS: ${aktifSayfa}`);
-		// renderToDoS()
+		
 	}
 
-
-	// /* export  */  function RenderSayfalayici(tbody, todos_count, aktifSayfa, sk) {
-	// 	const tr = document.createElement("tr")
-	// 	tr.innerHTML = RenderSayfalamaButtonlari(tbody, todos_count, aktifSayfa, sk)
-	// 	tbody.appendChild(tr)
-	// 	console.log("Sayfalayici:" + tr.innerHTML)
-	// }
-
 	const SayfaSayisi = () => {
-		console.log("			SayfaSayisi", parseInt(todos.length / SES), todos.length, SES)
-		return parseInt(todos.length / SES)
+		console.log("			SayfaSayisi", parseInt(todos.length / ses), todos.length, ses)
+		return parseInt(todos.length / ses)
 	};
 
 	/* export  */
 	const RenderSayfalamaButtonlari = (todos_count, kk) => {
-		console.log("			RenderSayfalamaButtonlari:3 ", todos_count, aktifSayfa, (SayfaSayisi() - aktifSayfa) > SES)
-		//root.innerHTML = ""
-		//  debugger
+		console.log("			RenderSayfalamaButtonlari:3 ", todos_count, aktifSayfa, (SayfaSayisi() - aktifSayfa) > ses)
 		return (
 			<>
 				<tr style={{ textAlign: "center" }}>
 					<td className="SayfalayiciTd" colSpan={5} >
 						<button className="btn btn-success" onClick={() => aktifSayfaDegis(1)} > İLK SAYFA</button>                    &nbsp; | &nbsp;
 						<button className="btn btn-success" onClick={() => aktifSayfaDegis(aktifSayfa - 5)} disabled={aktifSayfa - 5 > 0 ? `` : "disabled"} > -5 </button>                    &nbsp; | &nbsp;
-						{/* <button className="btn btn-success" onClick={() => aktifSayfaDegis(aktifSayfa - 5)} disabled={aktifSayfa - 1 > 0 ? `` : "disabled"} > -1 </button> */}
 
 						<button className="btn btn-warning" onClick={() => aktifSayfaDegis(aktifSayfa - 1)} disabled={aktifSayfa > 1 ? `` : "disabled"}> Önceki </button> &nbsp; | &nbsp;
 
@@ -239,64 +244,45 @@ const TodoList = () => {
 
 	//#region Sıralama 
 
-	/* export  */  const SiralamaTH = () => {
-		return (
-			<table className="table table-hover">
-				<thead>
-					<tr>
-						<th scope="col" style={{ width: "2cm" }} Id="idSiralamaKriteri_Id" onClick="SiralamaKriteri_Id(this)" >Id</th>
-						<th scope="col" style={{ width: "2cm" }} Id="idSiralamaKriteri_KId" onClick="SiralamaKriteri_KId(this)" >Şahıs Id</th>
-						<th scope="col" style={{ width: "4cm" }} Id="idSiralamaKriteri_Durum" onClick="SiralamaKriteri_Durum(this)" >Durum</th>
-						<th scope="col" style={{ width: "2cm" }} Id="idSiralamaKriteri_Baslik" onClick="SiralamaKriteri_Baslik(this)" >Başlık</th>
-						<th scope="col" style={{ width: "5cm" }} >İşlem</th>
-					</tr>
-				</thead>
-				<tbody></tbody>
-			</table>
-		)
-	}
-
-
-	/* export  */  function SiralamaKriteri_Baslik(e) {
-		console.log("SiralamaKriteri_Baslik", SiralayisKriteri);
+	function SiralamaKriteri_Baslik(e) {
+		console.log("SiralamaKriteri_Baslik", siralayisKriteri);
 		SayfalayiciSifirla()
-		SiralayisKriteri = 10 + (SiralayisKriteri % 2 == 0 ? 1 : 0)
-		/* renderTodos() */
+		setSiralayisKriteri(10 + (siralayisKriteri % 2 === 0 ? 1 : 0))
+		
 	}
 
-	/* export  */  function SiralamaAssist(root, todos) {
-		// for paging (0,15) => (n,n+15) olabilir
-		if (SiralayisKriteri > 0) { root.innerHTML = "" }
-		if (SiralayisKriteri === 10) { todos.sort((a, b) => { return a.title > b.title }) }
-		if (SiralayisKriteri === 11) { todos.sort((b, a) => { return a.title > b.title }) }
-		if (SiralayisKriteri === 20) { todos.sort((a, b) => { return a.id > b.id }) }
-		if (SiralayisKriteri === 21) { todos.sort((b, a) => { return a.id > b.id }) }
-		if (SiralayisKriteri === 30) { todos.sort((a, b) => { return a.completed > b.completed }) }
-		if (SiralayisKriteri === 31) { todos.sort((b, a) => { return a.completed > b.completed }) }
+	function SiralamaAssist(root) {
+		// debugger
+		if (siralayisKriteri === 11) { todos.sort((a, b) => { return a.title > b.title }) }
+		if (siralayisKriteri === 10) { todos.sort((b, a) => { return a.title > b.title }) }
+		if (siralayisKriteri === 21) { todos.sort((a, b) => { return a.id > b.id }) }
+		if (siralayisKriteri === 20) { todos.sort((b, a) => { return a.id > b.id }) }
+		if (siralayisKriteri === 31) { todos.sort((a, b) => { return a.completed > b.completed }) }
+		if (siralayisKriteri === 30) { todos.sort((b, a) => { return a.completed > b.completed }) }
 	}
 
-	/* export  */  const SiralamaKriteri_Id = (e) => {
-		console.log("SiralamaKriteri_Id", SiralayisKriteri);
+	const SiralamaKriteri_Id = (e) => {
+		console.log("SiralamaKriteri_Id", siralayisKriteri);
 		SayfalayiciSifirla()
-		SiralayisKriteri = 20 + (SiralayisKriteri % 2 == 0 ? 1 : 0)
-		// renderTodos()
+		setSiralayisKriteri(20 + (siralayisKriteri % 2 === 0 ? 1 : 0))
+		
 	}
 
-	/* export  */  const SiralamaKriteri_KId = (e) => {
-		console.log("SiralamaKriteri_KId", SiralayisKriteri);
+	const SiralamaKriteri_KId = (e) => {
+		console.log("SiralamaKriteri_KId", siralayisKriteri);
 		SayfalayiciSifirla()
-		SiralayisKriteri = 20 + (SiralayisKriteri % 2 == 0 ? 1 : 0)
-		// renderTodos()
+		setSiralayisKriteri(20 + (siralayisKriteri % 2 === 0 ? 1 : 0))
+		
 	}
 
-	/* export  */  const SiralamaKriteri_Durum = (e) => {
-		console.log("SiralamaKriteri_Durum", SiralayisKriteri);
+	const SiralamaKriteri_Durum = (e) => {
+		console.log("SiralamaKriteri_Durum", siralayisKriteri);
 		SayfalayiciSifirla()
-		SiralayisKriteri = 30 + (SiralayisKriteri % 2 == 0 ? 1 : 0)
-		// renderTodos()
+		setSiralayisKriteri(30 + (siralayisKriteri % 2 === 0 ? 1 : 0))
+		
 	}
 
-	/* export  */  const SayfalayiciSifirla = () => { aktifSayfa = 1 }
+	const SayfalayiciSifirla = () => { setAktifSayfa(1) }
 
 
 	//#endregion
